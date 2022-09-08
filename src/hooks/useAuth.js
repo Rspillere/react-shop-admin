5f0bbf01934de4eab9d894bfa8ceebda5aba1682
Rspillere <rspillere@gmail.com>
2022-09-08 00:16:51 -0300

    se optimió useAuth

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,16 +1,14 @@
-import React, { useState, useContext, createContext } from 'react';
-import Cookie from 'js-cookie'; //Nos ayuda asignar a nuestro navegador las cookies que esté recibiendo en el momento de la autenticación
-import axios from 'axios'; //Para el manejo de las peticiones como GET, PUT, POST, DELETE
+import React, { useState, useContext, createContext, useEffect } from 'react';
+import Cookie from 'js-cookie';
+import axios from 'axios';
 import endPoints from 'services/api';
 
-const AuthContext = createContext(); //Se crea un nuevo context gracias a la api de react
-//Se crea la encapsulación de nuestra aplicación
+const AuthContext = createContext();
 export function ProviderAuth({ children }) {
   const auth = useProvideAuth();
   return <AuthContext.Provider value={auth}>{children}</AuthContext.Provider>;
 }
 
-//Permite exponer cierta información que se estará requiriendo
 export const useAuth = () => {
   return useContext(AuthContext);
 };
@@ -18,25 +16,42 @@ export const useAuth = () => {
 //Captar la información del usuario
 function useProvideAuth() {
   const [user, setUser] = useState(null);
+  const token = Cookie.get('token');
 
-  const signIn = async (email, password) => {
-    const options = {
-      headers: {
-        accept: '*/*',
-        'Content-Type': 'application/json',
-      },
-    };
-    const {
-      data: { access_token: token },
-    } = await axios.post(endPoints.auth.login, { email, password }, options);
-
+  const checkToken = async () => {
     if (token) {
-      Cookie.set('token', token, { expires: 5 });
-
       axios.defaults.headers.Authorization = `Bearer ${token}`;
-
       const { data: user } = await axios.get(endPoints.auth.profile);
       setUser(user);
+    }
+  };
+
+  useEffect(() => {
+    checkToken();
+  }, []);
+
+  const signIn = async (email, password) => {
+    if (token) {
+      return;
+    } else {
+      const options = {
+        headers: {
+          accept: '*/*',
+          'Content-Type': 'application/json',
+        },
+      };
+      const {
+        data: { access_token: token },
+      } = await axios.post(endPoints.auth.login, { email, password }, options);
+
+      if (token) {
+        Cookie.set('token', token, { expires: 5 });
+
+        axios.defaults.headers.Authorization = `Bearer ${token}`;
+
+        const { data: user } = await axios.get(endPoints.auth.profile);
+        setUser(user);
+      }
     }
   };
 
